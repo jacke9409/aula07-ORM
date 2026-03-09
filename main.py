b8c08f8743c8e830366cba868c40912e52a1f5c2
Jackelyne <Jackelyneramosbrito@gmail.com>
2026-03-09 12:57:57 -0300

    instânciando banco de dados estoque

--- main.py
+++ main.py
@@ -8,3 +8,46 @@ from sqlalchemy import Column, Integer, String, Float, Boolean
 from sqlalchemy.orm import declarative_base
 # Importa a ferramenta para criar sessões de banco de dados
 from sqlalchemy.orm import sessionmaker
+
+# classe base para ORM (Object-Relational Mapping)
+Base = declarative_base()
+
+# classes = tabelas do banco de dados
+# objeto de cada classe é uma linha da tabela
+# atributos= colunas da tabela
+
+# classe produto representa a tabela 'produtos' no banco de dados
+class Produto(Base):
+    # nome da tabela no banco de dados
+    __tablename__ = "produtos"
+    # coluna de id
+    # Interger > números inteiros
+    # primary_key=True > define a coluna como chave primária
+    id = Column(Integer, primary_key=True)
+    # coluna de nome do produto
+    # string > texto
+    nome = Column(String(100),)
+    # coluna de preço do produto
+    # Float > números decimais
+    preco = Column(Float)
+    # metodo construtor
+    def __init__(self, nome, preco, estoque, ativo):
+        self.nome = nome
+        self.preco = preco
+        self.estoque = estoque
+        self.ativo = ativo
+    # quantidade em estoque
+    estoque = Column(Integer)
+    # Produto disponível para venda
+    ativo = Column(Boolean)
+
+    # Representação do objeto para imprimir
+    def __repr__(self):
+        return f"Produto(id={self.id}, nome={self.nome}, preco={self.preco}, estoque={self.estoque}, ativo={self.ativo})"
+
+# criar a conexão com o banco de dados
+# echo=True > log do sql
+engine = create_engine("sqlite:///estoque.db", echo=True)
+
+# criar as tabelas no banco de dados (se não existirem)
+Base.metadata.create_all(engine)
